2795e84194be2dc1159b297ac0a918a1e048802e
Kiana Jafari <kiana.jafari.ai@gmail.com>
2026-04-13 13:31:55 +0330

    Delete Projects/Garage System directory

--- Projects/Garage System/Display.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Structures.h"
-
-void DisplayVehicles(const std::array<Vehicle, MAX_VEHICLES>&, int&);
-void DisplayFreeSpots(const std::array<std::array<ParkingSpot, MAX_SPOTS>, LEVELS>&);
-void SearchVehicle(const std::array<Vehicle, MAX_VEHICLES>&, int&);
