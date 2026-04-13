f17bb9d5b5e64e76c514314383920fc78e2b3472
Kiana Jafari <kiana.jafari.ai@gmail.com>
2026-04-13 13:36:14 +0330

    Delete Projects/Garage directory

--- Projects/Garage/File.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "Structures.h"
-
-void ReadFromFile(std::array<Vehicle, MAX_VEHICLES>&, std::array<std::array<ParkingSpot, MAX_SPOTS>, LEVELS>&, int&);
-void SaveVehicles(const std::array<Vehicle, MAX_VEHICLES>&, int&);
