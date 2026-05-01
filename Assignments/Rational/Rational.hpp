3726b746aeff5700117fca4ef83e65b1c8cdda5a
Kiana Jafari <kiana.jafari.ai@gmail.com>
2026-05-01 17:44:01 +0330

    Create Rational.hpp

---
+++ Assignments/Rational/Rational.hpp
@@ -0,0 +1,40 @@
+#include <tuple>
+#include <string>
+
+#ifndef RATIONAL_H
+#define RATIONAL_H
+
+class Rational
+{
+
+public:
+
+    Rational() = default; // default constructor (for operator overloders)
+    Rational(const int, const unsigned int, const unsigned int);
+    ~Rational();
+
+    void setNumerator(const unsigned int);
+    void setDenominator(const unsigned int);
+    void setSign(const int);
+    int getSign() const;
+    unsigned int getNumerator() const;
+    unsigned int getDenominator() const;
+    std::string toString() const;
+
+    Rational operator+(const Rational&) const;
+    Rational operator-(const Rational&) const;
+    Rational operator*(const Rational&) const;
+    Rational operator/(const Rational&) const;
+
+private:
+
+    unsigned int a;
+    unsigned int b;
+    int sign; // could be -1 or +1 (negative & positive, respectively)
+
+    // utilities
+    void reducedForm(unsigned int&, unsigned int&) const;
+    std::tuple<unsigned int, unsigned int, unsigned int> getFactors(unsigned int, unsigned int) const;
+};
+
+#endif
