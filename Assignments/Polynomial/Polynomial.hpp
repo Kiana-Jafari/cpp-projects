5ccef0312ce0ee1ed90ecdec186504b75fc31ed4
Kiana Jafari <kiana.jafari.ai@gmail.com>
2026-05-01 10:48:40 +0330

    Create Polynomial.hpp

---
+++ Assignments/Polynomial/Polynomial.hpp
@@ -0,0 +1,27 @@
+#include <array>
+#include <string>
+
+#ifndef POLYNOMIAL_H
+#define POLYNOMIAL_H
+
+class Polynomial {
+
+public:
+
+    static const size_t maxDegree{100};
+
+    Polynomial();
+    ~Polynomial();
+
+    void getPolynomial();
+    std::string printPolynomial();
+    Polynomial operator+(const Polynomial&) const;
+    Polynomial operator*(const Polynomial&) const;
+
+private:
+
+    int n; // polynomial's degree
+    std::array<int, maxDegree> factor; // coefficients of the polynomial
+};
+
+#endif
